e9149d5fd9f948f217e333b31c375feecba374f4
snowsan0113 <egawayuuki0113@gmail.com>
2025-07-28 09:04:38 +0900

    【追加】スクリーンショットする場所に目印の図形を設置

--- src/main/java/snowsan0113/clipscreen/Main.java
+++ src/main/java/snowsan0113/clipscreen/Main.java
@@ -3,7 +3,10 @@ package snowsan0113.clipscreen;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.scene.shape.Rectangle;
 import snowsan0113.clipscreen.manager.ScreenshotManager;
 import snowsan0113.clipscreen.util.Location;
 
@@ -33,7 +36,7 @@ public class Main extends Application {
         int width = displayMode.getWidth();
         int height = displayMode.getHeight();
 
-        HBox screen_pane = new HBox();
+        Pane screen_pane = new Pane();
         Scene screen_scene = new Scene(screen_pane, width, height);
         Stage screen_stage = new Stage();
         screen_stage.setOpacity(0.2);
@@ -42,12 +45,24 @@ public class Main extends Application {
         screen_stage.show();
 
         screen_scene.setOnMouseClicked(e -> {
+            Circle start_circle = null;
+            Circle end_circle = null;
             if (start_clip == null && end_clip == null) {
                 start_clip = new Location(e.getScreenX(), e.getScreenY(), 0);
+                start_circle = new Circle(start_clip.x(), start_clip.y(), 10);
+                screen_pane.getChildren().add(start_circle);
                 System.out.println("スタートでクリックした場所は" + start_clip.x() + "," + start_clip.y() + "," + start_clip.z());
             }
             else if (start_clip != null && end_clip == null) {
                 end_clip = new Location(e.getScreenX(), e.getScreenY() ,0);
+                end_circle = new Circle(end_clip.x(), end_clip.y(), 10);
+                double sc_x = Math.min(start_clip.x(), end_clip.x());
+                double sc_y = Math.min(start_clip.y(), end_clip.y());
+                double sc_w = Math.abs(start_clip.x() - end_clip.x());
+                double sc_h = Math.abs(start_clip.y() - end_clip.y());
+                Rectangle clip_area = new Rectangle((int) sc_x, (int) sc_y, (int) sc_w, (int) sc_h);
+                screen_pane.getChildren().add(clip_area);
+                screen_pane.getChildren().add(end_circle);
                 System.out.println("エンドでクリックした場所は" + end_clip.x() + "," + end_clip.y() + "," + end_clip.z());
             }
             else if (start_clip != null && end_clip != null) {
